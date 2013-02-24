843b0dc580c3cb833452d0913713e0d2df6751b4
Elfmagi <elfmagi@live.com>
2013-02-24 17:11:34 -0600

    Cleaned up the code some

--- launcher.tcl
+++ launcher.tcl
@@ -1,5 +1,35 @@
 # Launcher for Project Junipero
 # version 0.0.1 20130220
+
+#TitleName
+
+message .msg0 -text "Project Junipero"
+
+#UserName PassWord Entry
+
+label .label0 -text "Username:"
+
+entry .entry0 -textvar username
+
+label .label1 -text "Password:"
+
+entry .entry1 -textvar password
+
+set usernamedb username.tcl
+
+#LoginButton
+
+button .button0 -command { 
+	if  {$username == "testname"} && {$password == "test123"}
+	[source game.tcl]
+	else [-text "Hmm. You don't seem to have an account, or you entered your account details wrong."]
+} -text "Login"
+
+pack .msg0 .label0 .entry0 .label1 .entry1 .button0
+
+#ChangeLog
+# 20130224 Cleaned up notations, marked out the clean procedure, cleaned up code in general to be more visually appealing.
+
 #temp clean
 #proc clean {} {
 #	for {set i 0} {$i <= 2147} {incr i} {
@@ -10,20 +40,3 @@
 #	}
 #}
 #clean
-
-message .msg0 -text "Project Junipero"
-
-#entry widgets
-label .label0 -text "Username:"
-entry .entry0 -textvar username
-label .label1 -text "Password:"
-entry .entry1 -textvar password
-set usernamedb username.tcl
-#buttons
-button .button0 -command { 
-	if  {$username == "testname"} && {$password == "test123"}
-	[source game.tcl]
-	else [-text "Hmm. You don't seem to have an account, or you entered your account details wrong."]
-} -text "Login"
-
-pack .msg0 .label0 .entry0 .label1 .entry1 .button0
