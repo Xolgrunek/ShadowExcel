6498d5c958b001d6f28471e547cd295c0d4526b3
Jason Slightom <smithogon@hotmail.com>
2013-02-24 22:13:10 -0600

    Bound <Return> key to button0
    fixed the if elseif else statement (again) added in a configurable .msg1
    widget for error codes.

--- launcher.tcl
+++ launcher.tcl
@@ -17,16 +17,22 @@ entry .entry1 -textvar password
 
 set usernamedb username.tcl
 
+
+#Just some text
+
+message .msg1
+
 #LoginButton
 
 button .button0 -command { 
-	if  {$username == "testname"} && {$password == "test123"}
+	if  {$username == "testname"} {
 	[source game.tcl]
-	elseif {$username !== "testname"} [-text "Hmm. You don't seem to have an account, or you entered your account details wrong."]
-	else [-text "You broke it 
+	} elseif {$username !== "testname"} {[.msg1 configure -text "Hmm. You don't seem to have an account, or you entered your account details wrong."]
+	} else [.msg1 configure -text "You broke it] 
 } -text "Login"
 
-pack .msg0 .label0 .entry0 .label1 .entry1 .button0
+bind . <Return> ".button0 invoke"
+pack .msg0 .msg1 .label0 .entry0 .label1 .entry1 .button0
 
 #ChangeLog
 # 20130224 Cleaned up notations, marked out the clean procedure, cleaned up code in general to be more visually appealing.
